a0b862aaad4592204007229c50a0c3b4e98e4033
Lucas6331 <lukasspfc8@gmail.com>
2023-08-31 21:59:03 -0300

    feat: added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './template/header/header.component';
 import { FooterComponent } from './template/footer/footer.component';
-import { HomeComponent } from './template/home/home.component';
+import { HomeComponent } from './components/home/home.component';
+import { ListarProdutosComponent } from './components/produtos/listar-produtos/listar-produtos.component';
+import { CadastrarProdutoComponent } from './components/produtos/cadastrar-produto/cadastrar-produto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    ListarProdutosComponent,
+    CadastrarProdutoComponent
   ],
   imports: [
     BrowserModule,
